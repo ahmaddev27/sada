daadbc025e1c74ba774bb61f9b9e6410b0dc4253
ahmaddev27 <aa20589@gmail.com>
2026-04-29 12:30:00 +0300

    fix(auth): add stateless() to Google OAuth to fix InvalidStateException on HTTP

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -6,6 +6,7 @@ use App\Actions\Auth\AuthenticateWithGoogleAction;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\RedirectResponse;
 use Laravel\Socialite\Facades\Socialite;
+use Laravel\Socialite\Two\AbstractProvider;
 use Symfony\Component\HttpFoundation\RedirectResponse as SymfonyRedirect;
 
 // AUTH-02: Google OAuth via Socialite
@@ -13,12 +14,17 @@ class GoogleController extends Controller
 {
     public function redirect(): SymfonyRedirect|RedirectResponse
     {
-        return Socialite::driver('google')->redirect();
+        /** @var AbstractProvider $driver */
+        $driver = Socialite::driver('google');
+
+        return $driver->stateless()->redirect();
     }
 
     public function callback(AuthenticateWithGoogleAction $action): RedirectResponse
     {
-        $googleUser = Socialite::driver('google')->user();
+        /** @var AbstractProvider $driver */
+        $driver     = Socialite::driver('google');
+        $googleUser = $driver->stateless()->user();
         $user       = $action->execute($googleUser);
 
         auth()->login($user, remember: true);
